456916f5f5ac7552fd58354123b3ba2a098b0766
TytoAlba <Tytoalbajjfm@gmail.com>
2025-05-26 00:24:18 +0200

    Volví A solucionar

--- The-Valkirist-Demo/Assets/Scripts/DontDestroyOnLoad.cs
+++ The-Valkirist-Demo/Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,30 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/GameController.cs
-public class GameController : MonoBehaviour
-========
-public class DontDestroyOnLoad : MonoBehaviour
->>>>>>>> 72168df012b730fc6774fc99e1ee197d41545ba6:The-Valkirist-Demo/Assets/Scripts/DontDestroyOnLoad.cs
+public class PersistenteUnico : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-<<<<<<<< HEAD:Assets/Scripts/GameController.cs
-    
-========
+    private static PersistenteUnico instancia;
 
+    private void Awake()
+    {
+        if (instancia == null)
         {
+            instancia = this;
             DontDestroyOnLoad(gameObject);
         }
-
->>>>>>>> 72168df012b730fc6774fc99e1ee197d41545ba6:The-Valkirist-Demo/Assets/Scripts/DontDestroyOnLoad.cs
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
