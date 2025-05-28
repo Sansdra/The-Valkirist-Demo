23d9b01139173b034c48a3010b6760b3d7ea0ead
TytoAlba <Tytoalbajjfm@gmail.com>
2025-05-28 18:37:50 +0200

    Cambios_escenaJuego

---
+++ Assets/Scripts/EscenaJuego/VueltaHabitacion.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VueltaHabitacion : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneTransitionManager.Instance.GoToScene("Habitacion");
+        }
+    }
+}
